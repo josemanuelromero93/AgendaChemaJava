49080a8927ef2f2bfa70c94d154af3dd5e79e43e
Chema <josemanuelromero93@outlook.com>
2017-01-29 16:32:38 +0100

    Added Help

---
+++ src/com/chema/Help.java
@@ -0,0 +1,17 @@
+package com.chema;
+
+
+public class Help {
+
+    public static void printHelp() {
+
+
+        System.out.println("Los comandos válidos son");
+        System.out.println("(Q)uit:\t\t Para salir de la agenda");
+        System.out.println("(H)elp:\t\t Para mostrar la ayuda");
+        System.out.println("(L)ist:\t\t Para mostrar la lista de contactos");
+        System.out.println("(A)dd:\t\t Para crear un contacto nuevo");
+        System.out.println("(D)elete:\t\t Para eliminar un contacto");
+
+    }
+}
